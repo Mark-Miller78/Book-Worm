dc9deeedd92deb7a53cbc28acf07c9852914cfa9
Mark Miller <markmiller7858@gmail.com>
2022-03-28 22:12:32 -0400

    can add user, working on auth

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const path = require('path');
 const {ApolloServer} =require('apollo-server-express');
 const {authMiddleware} = require('./utils/auth');
 
+const {typeDefs, resolvers} = require('./schemas');
+
 const db = require('./config/connection');
 
 
@@ -30,13 +32,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '../client/build')));
+// }
 
-app.get('*', (req,res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
+// app.get('*', (req,res) => {
+//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+// });
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`API server running on port ${PORT}!`));
